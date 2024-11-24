3fe7e5c0795175133a699cec3dbb3ffeeead2691
thankyou0 <parthivva227@gmail.com>
2024-11-25 04:40:59 +0530

    vercel

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const config = {
   // PWD_SECRET: process.env.REACT_APP_PWD_SECRET
   // BACKEND_API :'https://deploy-news.vercel.app',
   // BACKEND_API:'https://netlify-news-backend.netlify.app',
-  BACKEND_API: 'https://normal-backend-render.onrender.com',
-  // BACKEND_API: 'https://normal-backend.vercel.app',
+  // BACKEND_API: 'https://normal-backend-render.onrender.com',
+  BACKEND_API: 'https://normal-backend.vercel.app',
   PWD_SECRET: 'news-aggregator-secret'
 };
 
